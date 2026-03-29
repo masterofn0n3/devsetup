62fb8a9784d9eb709a23c6f34a34e58b32ecb1e5
Anh Duong Tien <anhdt1911.work@gmail.com>
2026-03-29 12:22:26 +0800

    add alt+arrow key to move lines feature similar to vscode

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -7,3 +7,14 @@ vim.keymap.set("n", "<C-s>", "<cmd>w<cr>", { desc = "Save" })
 vim.keymap.set("i", "<C-s>", "<C-o><cmd>w<cr>", { desc = "Save" })
 vim.keymap.set("v", "<C-s>", "<C-c><cmd>w<cr>", { desc = "Save" })
 vim.keymap.set("i", "<M-BS>", "<C-w>", { noremap = true })
+-- Move line down
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
+
+-- Move line up
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
+
+-- Move selected lines down
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+
+-- Move selected lines up
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
